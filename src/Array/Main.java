dd0839d23484716dcb9945bf22ffb77556427ec9
jeong <jeong.junhueck@gmail.com>
2024-05-22 23:25:40 +0900

    wednesday

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -1,5 +1,10 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -21,4 +26,38 @@ class ArrayMagic {
         }
         return answer;
     }
+
+    public int findSecondLargestElement(int[] array) {
+        int answer = -1;
+        if(array.length > 1){
+            boolean allEqual = Arrays.stream(array).distinct().limit(2).count() <= 1;
+            if(!allEqual) {
+                int max = Arrays.stream(array).max().getAsInt();
+                List<Integer> newList = Arrays.stream(array)
+                        .filter(x -> x < max)
+                        .boxed()
+                        .collect(Collectors.toList());
+                answer = Collections.max(newList);
+            }
+        }
+        return answer;
+    }
+
+//    int largestElement = Integer.MIN_VALUE;
+//    int secondLargestElement = Integer.MIN_VALUE;
+//
+//    for(int value: array) {
+//        if(value > largestElement) {
+//            secondLargestElement = largestElement;
+//            largestElement = value;
+//        } else if (value > secondLargestElement && value != largestElement) {
+//            secondLargestElement = value;
+//        }
+//    }
+//
+//    if(secondLargestElement == Integer.MIN_VALUE) {
+//        return -1;
+//    }
+
+
 }
